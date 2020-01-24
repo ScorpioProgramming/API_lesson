71f912bf6d0e36ea74ea7e32dca45aa043942873
Elev <Elev@TEB127022>
2020-01-24 14:49:22 +0100

    testing things

--- API_lesson/Program.cs
+++ API_lesson/Program.cs
@@ -13,12 +13,42 @@ namespace API_lesson
     {
         static void Main(string[] args)
         {
-            var client = new RestClient("http://");
+
+
+            var client = new RestClient("https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/16.158/lat/58.5812/data.json");
             var request = new RestRequest("/", Method.GET);
             IRestResponse response = client.Execute(request);
             String content = response.Content;
-            Menu menu = JsonConvert.DeserializeObject<Menu>(content);
-            
+            RootObject rootobject = JsonConvert.DeserializeObject<RootObject>(content);
+        }
+
+        public class Geometry
+        {
+            public string type { get; set; }
+            public List<List<double>> coordinates { get; set; }
+        }
+
+        public class Parameter
+        {
+            public string name { get; set; }
+            public string levelType { get; set; }
+            public int level { get; set; }
+            public string unit { get; set; }
+            public List<double> values { get; set; }
+        }
+
+        public class TimeSery
+        {
+            public DateTime validTime { get; set; }
+            public List<Parameter> parameters { get; set; }
+        }
+
+        public class RootObject
+        {
+            public DateTime approvedTime { get; set; }
+            public DateTime referenceTime { get; set; }
+            public Geometry geometry { get; set; }
+            public List<TimeSery> timeSeries { get; set; }
         }
     }
 }
