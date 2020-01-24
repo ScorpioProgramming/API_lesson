117135b82bdce77d5c63c3191a8b1ea7640b5802
Elev <Elev@TEB127022>
2020-01-24 14:29:05 +0100

    Add project files.

---
+++ API_lesson/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestSharp;
+
+
+namespace API_lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient("http://");
+            var request = new RestRequest("/", Method.GET);
+            IRestResponse response = client.Execute(request);
+            String content = response.Content;
+            Menu menu = JsonConvert.DeserializeObject<Menu>(content);
+        }
+    }
+}
